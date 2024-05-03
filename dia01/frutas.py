ad095118b62d4020eb4e834e74a4f996aa264f3f
Teo Calvo <teocalvo2@gmail.com>
2024-05-03 10:05:21 -0300

    Frutas template

---
+++ dia01/frutas.py
@@ -0,0 +1,15 @@
+# %%
+import pandas as pd
+
+df = pd.read_excel("../data/dados_frutas.xlsx")
+df
+
+# %%
+## Como aplicar o método do slide para descobrir a fruta?
+
+# <Escreva seu código aqui>
+
+# %%
+## Como podemos fazer a máquina aprender?
+
+# <Machine Learning>
